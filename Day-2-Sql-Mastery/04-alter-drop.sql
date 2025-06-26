cd6696ba12acc7d94cf01e96dd07c8543111674c
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-26 19:22:22 +0530

    Day2: Alter employees table and drop / delete column name email

--- Day-2-Sql-Mastery/04-alter-drop.sql
+++ Day-2-Sql-Mastery/04-alter-drop.sql
@@ -6,3 +6,6 @@ alter table employees add column email varchar(100);
 
 -- Modify existing column
 alter table employees modify column salary decimal(12,2);
+
+-- Drop column
+alter table employees drop column email;
