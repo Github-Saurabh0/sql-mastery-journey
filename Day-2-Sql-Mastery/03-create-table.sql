590687b421d1cbcbfc676c2fac3f5a89d8b0d535
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-26 19:15:10 +0530

    Day2: Create table employees and columns

--- Day-2-Sql-Mastery/03-create-table.sql
+++ Day-2-Sql-Mastery/03-create-table.sql
@@ -4,3 +4,13 @@ create table departments(
 dept_id int primary key,
 dept_name varchar(50) not null
 );
+
+
+create table employees(
+emp_id int primary key,
+emp_name varchar(50) not null,
+department_id Int,
+salary decimal(10,2),
+hire_date date,
+foreign key (department_id) references departments(dept_id)
+);
