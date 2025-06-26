d4479032386c247daccc2ba043709660396fb784
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-26 18:46:12 +0530

    Day2: create constraint demo table and create columns

--- Day-2-Sql-Mastery/02-constraints.sql
+++ Day-2-Sql-Mastery/02-constraints.sql
@@ -4,5 +4,5 @@ create table constraint_demo(
 id int primary key,
 username varchar(50) unique not null,
 age int check (age >= 18),
-country varchar(50) default 'India'
+country varchar(100) default 'India'
 );
