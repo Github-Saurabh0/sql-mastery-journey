12859be33b23f5d35f4a3be108628d691a772b5e
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-26 19:21:02 +0530

    Day2: Alter employees table and modify salary column

--- Day-2-Sql-Mastery/04-alter-drop.sql
+++ Day-2-Sql-Mastery/04-alter-drop.sql
@@ -1,3 +1,8 @@
 use sql_mastery_day2;
 
+
+-- Add a new column
 alter table employees add column email varchar(100);
+
+-- Modify existing column
+alter table employees modify column salary decimal(12,2);
