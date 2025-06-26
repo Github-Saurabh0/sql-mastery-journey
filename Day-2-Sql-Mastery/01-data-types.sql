b149b60a9081546dc354aa4f86241cbbeab45076
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-26 18:30:14 +0530

    Day2: create table and add some data types fields

--- Day-2-Sql-Mastery/01-data-types.sql
+++ Day-2-Sql-Mastery/01-data-types.sql
@@ -1,2 +1,11 @@
 create database if not exists sql_mastery_day2;
 use sql_mastery_day2;
+create table data_types_demo(
+id int,
+salary decimal(10,2),
+mark float,
+name varchar(50),
+description text,
+created_at date,
+Login_time timestamp
+);
