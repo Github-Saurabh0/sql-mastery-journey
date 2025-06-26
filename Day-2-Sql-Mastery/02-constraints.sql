84a87ae0dfbba20059076ecc1c1bf017f869c643
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-26 18:45:44 +0530

    Day2: create table and add some data types fields

---
+++ Day-2-Sql-Mastery/02-constraints.sql
@@ -0,0 +1,8 @@
+use sql_mastery_day2;
+
+create table constraint_demo(
+id int primary key,
+username varchar(50) unique not null,
+age int check (age >= 18),
+country varchar(50) default 'India'
+);
