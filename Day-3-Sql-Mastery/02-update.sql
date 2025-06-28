d7818634b16b1a91b4358e4d250b590f0f0e8fd7
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-28 15:43:11 +0530

    Update department of a specific employee using Where/And

--- Day-3-Sql-Mastery/02-update.sql
+++ Day-3-Sql-Mastery/02-update.sql
@@ -4,3 +4,10 @@ USE sql_mastery_day2;
 UPDATE employees
 SET salary = 80000.00
 WHERE emp_id = 101;
+
+
+-- Update department of a specific employee
+UPDATE employees
+SET department_id = 2
+WHERE emp_name = 'Riya Sen'
+And emp_id = 103;
