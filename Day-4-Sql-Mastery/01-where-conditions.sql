9f5b7b7c248812ac7b71fff2009025a61ea0725d
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-29 17:35:44 +0530

    Employees with salary < 65000 or in department 3

--- Day-4-Sql-Mastery/01-where-conditions.sql
+++ Day-4-Sql-Mastery/01-where-conditions.sql
@@ -7,3 +7,7 @@ WHERE department_id = 1;
 -- Employees with salary > 60000 and department = 1
 SELECT * FROM employees
 WHERE salary > 60000 AND department_id = 1;
+
+-- Employees with salary < 65000 or in department 3
+SELECT * FROM employees
+WHERE salary < 65000 OR department_id = 3;
