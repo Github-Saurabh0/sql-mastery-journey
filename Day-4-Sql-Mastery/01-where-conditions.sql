9ede23add4a9e243168248db59b14b501cf26f34
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-29 17:27:36 +0530

    Employees with salary > 60000 and department = 1

--- Day-4-Sql-Mastery/01-where-conditions.sql
+++ Day-4-Sql-Mastery/01-where-conditions.sql
@@ -3,3 +3,7 @@ USE sql_mastery_day2;
 -- Get employees in department 1
 SELECT * FROM employees
 WHERE department_id = 1;
+
+-- Employees with salary > 60000 and department = 1
+SELECT * FROM employees
+WHERE salary > 60000 AND department_id = 1;
