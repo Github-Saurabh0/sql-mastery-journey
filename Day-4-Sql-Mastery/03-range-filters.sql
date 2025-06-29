d55bc56d658a9db68ca4083480a4e84d77df2865
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-29 18:02:33 +0530

    Filter using IN

--- Day-4-Sql-Mastery/03-range-filters.sql
+++ Day-4-Sql-Mastery/03-range-filters.sql
@@ -3,3 +3,7 @@ USE sql_mastery_day2;
 -- Salary between 60000 and 80000
 SELECT * FROM employees
 WHERE salary BETWEEN 60000 AND 80000;
+
+-- Filter using IN
+SELECT * FROM employees
+WHERE department_id IN (1, 2);
