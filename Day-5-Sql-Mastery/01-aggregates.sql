65bcd9a4d233446464deb5d50a005c5b041e97e5
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-30 17:15:56 +0530

    Total salary of all employees

--- Day-5-Sql-Mastery/01-aggregates.sql
+++ Day-5-Sql-Mastery/01-aggregates.sql
@@ -2,3 +2,7 @@ USE sql_mastery_day2;
 
 -- Total number of employees
 SELECT COUNT(*) AS total_employees FROM employees;
+
+
+-- Total salary of all employees
+SELECT SUM(salary) AS total_salary FROM employees;
