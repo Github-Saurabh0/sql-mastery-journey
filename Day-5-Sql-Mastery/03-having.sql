74fea73002e98d1e738faa9edfe3f26d29ea5f63
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-30 17:27:34 +0530

    Departments with avg salary above 65000

--- Day-5-Sql-Mastery/03-having.sql
+++ Day-5-Sql-Mastery/03-having.sql
@@ -5,3 +5,10 @@ SELECT department_id, COUNT(*) AS employee_count
 FROM employees
 GROUP BY department_id
 HAVING COUNT(*) > 1;
+
+
+-- Departments with avg salary above 60000
+SELECT department_id, AVG(salary) AS avg_sal
+FROM employees
+GROUP BY department_id
+HAVING AVG(salary) > 65000;
