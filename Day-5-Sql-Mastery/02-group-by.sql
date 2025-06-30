2ea872734ba1468e7e39a3d1938b089361d7b363
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-30 17:23:37 +0530

    Average salary in each department

--- Day-5-Sql-Mastery/02-group-by.sql
+++ Day-5-Sql-Mastery/02-group-by.sql
@@ -4,3 +4,9 @@ USE sql_mastery_day2;
 SELECT department_id, COUNT(*) AS dept_employee_count
 FROM employees
 GROUP BY department_id;
+
+
+-- Average salary in each department
+SELECT department_id, AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department_id;
