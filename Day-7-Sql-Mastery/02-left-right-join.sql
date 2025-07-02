25944e2bdc7064b52cf9c84cefeb466333807b74
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-02 15:42:19 +0530

    LEFT JOIN: All employees + dept name (even if no matching department)

--- Day-7-Sql-Mastery/02-left-right-join.sql
+++ Day-7-Sql-Mastery/02-left-right-join.sql
@@ -1,7 +1,10 @@
 USE sql_mastery_day2;
 
+-- LEFT JOIN → Sabhi left table rows (employees) laata hai, chahe right match ho ya na ho.
+
 -- LEFT JOIN: All employees + dept name (even if no matching department)
 SELECT e.emp_id, e.emp_name, d.dept_name
 FROM employees e
 LEFT JOIN departments d
 ON e.department_id = d.dept_id;
+
