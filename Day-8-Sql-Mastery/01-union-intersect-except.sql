dff0bc69fa3c814b678901d2f0e5934466acc843
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-03 15:54:03 +0530

    Insert some records into employees_temp

--- Day-8-Sql-Mastery/01-union-intersect-except.sql
+++ Day-8-Sql-Mastery/01-union-intersect-except.sql
@@ -6,3 +6,9 @@ CREATE TABLE employees_temp (
     emp_name VARCHAR(100),
     department_id INT
 );
+
+-- Insert some records into employees_temp
+INSERT INTO employees_temp (emp_id, emp_name, department_id) VALUES
+(201, 'Saurabh Kushwaha', 1),
+(202, 'Aman Gupta', 4),
+(203, 'Riya Sen', 2);
