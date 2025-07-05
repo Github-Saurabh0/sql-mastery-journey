3997719bcf8d81fa8cc671e42339625d0a12c636
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-05 17:30:16 +0530

    ROLLBACK purane state me data ko wapas le jata hai — jab tak commit nahi hua ho, data revert ho sakta hai

--- Day-10-Sql-Mastery/02-rollback-example.sql
+++ Day-10-Sql-Mastery/02-rollback-example.sql
@@ -10,3 +10,6 @@ WHERE emp_name = 'Saurabh Kushwaha';
 
 -- Realize the mistake → rollback
 ROLLBACK;
+
+
+-- ROLLBACK purane state me data ko wapas le jata hai — jab tak commit nahi hua ho, data revert ho sakta hai.
