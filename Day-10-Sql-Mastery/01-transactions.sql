a6428c5dc9a9e89edb5e0d23c0d26163af24070c
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-05 17:20:03 +0530

    Deduct ₹5000 from Saurabh's salary

--- Day-10-Sql-Mastery/01-transactions.sql
+++ Day-10-Sql-Mastery/01-transactions.sql
@@ -3,4 +3,8 @@ USE sql_mastery_day2;
 -- Start a transaction manually
 START TRANSACTION;
 
+-- Deduct ₹5000 from Saurabh's salary
+UPDATE employees
+SET salary = salary - 5000
+WHERE emp_name = 'Saurabh Kushwaha';
 
