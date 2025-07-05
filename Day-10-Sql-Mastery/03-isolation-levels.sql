42ef753738669c3cfb8e353d4cbb8b308d804b78
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-05 17:38:42 +0530

    Start transaction => Query execute => End transaction

--- Day-10-Sql-Mastery/03-isolation-levels.sql
+++ Day-10-Sql-Mastery/03-isolation-levels.sql
@@ -4,8 +4,11 @@ SELECT @@transaction_isolation;
 -- Set isolation level
 SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
+-- Start transaction
+START TRANSACTION;
 
+-- Example query
+SELECT * FROM employees WHERE department_id = 1;
 
-
-
-
+-- End transaction
+COMMIT;
