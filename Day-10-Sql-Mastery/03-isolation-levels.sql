3ea4798367a45dd760e0246fbfe1cb0cb66d7c75
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-05 17:39:42 +0530

    Isolation Level decides how safely transactions run without interfering each other

--- Day-10-Sql-Mastery/03-isolation-levels.sql
+++ Day-10-Sql-Mastery/03-isolation-levels.sql
@@ -12,3 +12,13 @@ SELECT * FROM employees WHERE department_id = 1;
 
 -- End transaction
 COMMIT;
+
+-- Isolation Level decides how safely transactions run without interfering each other.
+
+-- Isolation Level	Prevents Problems	Use Case Example
+-- READ UNCOMMITTED	Dirty reads	Rarely used
+-- READ COMMITTED	Prevents dirty reads	Default in most DBs
+-- REPEATABLE READ	Prevents non-repeatable reads	InnoDB default (MySQL)
+-- SERIALIZABLE	Prevents phantom reads	High consistency, low concurrency
+
+
